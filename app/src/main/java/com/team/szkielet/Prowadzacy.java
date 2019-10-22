afc61392f6ff9b37c6f2a6113e7880b012f3c6a0
Bonniu <radekg23@o2.pl>
2019-10-22 09:49:08 +0200

    Lecturers first layout

--- app/src/main/java/com/team/szkielet/Prowadzacy.java
+++ app/src/main/java/com/team/szkielet/Prowadzacy.java
@@ -8,9 +8,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import org.w3c.dom.Document;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+
 public class Prowadzacy extends AppCompatActivity {
+
+    EditText etName;
+    EditText etSurname;
+    Button btnSearch;
+    WebView wwProw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +41,29 @@ public class Prowadzacy extends AppCompatActivity {
         //actionBar.setIcon();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
+        etName = findViewById(R.id.etName);
+        etSurname = findViewById(R.id.etSurname);
+        btnSearch = findViewById(R.id.btnSearch);
+        wwProw = findViewById(R.id.wwProw);
+        /// nie wiem po co to ale bez tego nie dziala
+        wwProw.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = etName.getText().toString().trim();
+                String surname = etSurname.getText().toString().trim();
+                String googleSearch = "https://adm.edu.p.lodz.pl/user/users.php?search=" + name + "+" + surname;
+                wwProw.loadUrl(googleSearch);
+                System.out.println("URL: " + googleSearch);
+                System.out.println("loaded");
+            }
+        });
+
     }
 
     @Override
@@ -31,22 +74,19 @@ public class Prowadzacy extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId() == R.id.plany) {
+        if (item.getItemId() == R.id.plany) {
             //Toast.makeText(Prowadzacy.this, "Plany zajęć", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(Prowadzacy.this, Plany.class);
             startActivity(intent);
-        }
-        else if(item.getItemId() == R.id.prowadzacy) {
+        } else if (item.getItemId() == R.id.prowadzacy) {
             /*Toast.makeText(Prowadzacy.this, "Prowadzący", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(Prowadzacy.this, Prowadzacy.class);
             startActivity(intent);*/
-        }
-        else if(item.getItemId() == R.id.aktualnosci) {
+        } else if (item.getItemId() == R.id.aktualnosci) {
             //Toast.makeText(Prowadzacy.this, "Aktualności", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(Prowadzacy.this, Aktualnosci.class);
             startActivity(intent);
-        }
-        else if(item.getItemId() == R.id.start) {
+        } else if (item.getItemId() == R.id.start) {
             //Toast.makeText(Prowadzacy.this, "Menu główne", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(Prowadzacy.this, MainActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
