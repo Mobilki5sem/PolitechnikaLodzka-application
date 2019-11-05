c9ae07d85a126736eea7665e18fafb3e90296e92
Kar0lLasek <19karolasek98@gmail.com>
2019-11-05 22:02:44 +0100

    Add checking link. Better toasts. OnItemClick in RecyclerView.

--- app/src/main/java/com/team/szkielet/event/AddEvent.java
+++ app/src/main/java/com/team/szkielet/event/AddEvent.java
@@ -2,12 +2,16 @@ package com.team.szkielet.event;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -29,14 +33,18 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AddEvent extends AppCompatActivity {
 
     private EditText etNazwaWydarzenia, etOpis, etLink;
     private CheckBox cbMamWydarzenie;
     private RadioGroup rgRodzaj;
-    private Button btnAddEvent;
+    private Button btnAddEvent, btnCheckLink;
     private RadioButton rbChecked;
+
+    private boolean ifLinkWasChecked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,12 +61,35 @@ public class AddEvent extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if(cbMamWydarzenie.isChecked()) {
                     etLink.setVisibility(View.VISIBLE);
+                    btnCheckLink.setVisibility(View.VISIBLE);
                 } else {
                     etLink.setVisibility(View.GONE);
+                    btnCheckLink.setVisibility(View.GONE);
                 }
             }
         });
         rgRodzaj = findViewById(R.id.rgRodzaj);
+        btnCheckLink = findViewById(R.id.btnCheckLink);
+        btnCheckLink.setVisibility(View.GONE);
+        btnCheckLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String urlReally = etLink.getText().toString();
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                try {
+                    i.setData(Uri.parse(urlReally));
+                    startActivity(i);
+                    Toast.makeText(AddEvent.this, "Link jest prawidłowy.", Toast.LENGTH_LONG).show();
+                    ifLinkWasChecked = true;
+                } catch (Exception ex) {
+                    Toast.makeText(AddEvent.this, "Link jest nieprawidłowy.", Toast.LENGTH_LONG).show();
+                    ifLinkWasChecked = false;
+                }
+
+
+            }
+        });
+
         btnAddEvent = findViewById(R.id.btnAddEvent);
 
         btnAddEvent.setOnClickListener(new View.OnClickListener() {
@@ -101,9 +132,6 @@ public class AddEvent extends AppCompatActivity {
                         }
                     }).start();
                 }
-                else {
-                    Toast.makeText(AddEvent.this, "Wypełnij wszystkie potrzebne pola", Toast.LENGTH_SHORT).show();
-                }
             }
         });
     }
@@ -124,21 +152,42 @@ public class AddEvent extends AppCompatActivity {
     }
 
     private boolean[] checkIfAllWasFilled() {
+        Toast toast;
         boolean tab[] = {false, false, false, false};
+
+        if(!etNazwaWydarzenia.getText().toString().equals(""))
+            tab[0] = true;
+        else {
+            Toast.makeText(AddEvent.this, "Dodaj nazwę wydarzenia.", Toast.LENGTH_SHORT).show();
+        }
+        if(!etOpis.getText().toString().equals(""))
+            tab[1] = true;
+        else {
+            Toast.makeText(AddEvent.this, "Dodaj opis wydarzenia.", Toast.LENGTH_SHORT).show();
+        }
         if (rgRodzaj.getCheckedRadioButtonId() != -1) {
             tab[2] = true;
             int rbID = rgRodzaj.getCheckedRadioButtonId();
             rbChecked = findViewById(rbID);
+        } else {
+            Toast.makeText(AddEvent.this, "Wybierz rodzaj wydarzenia.", Toast.LENGTH_SHORT).show();
         }
-
-
-        if(!etNazwaWydarzenia.getText().toString().equals(""))
-            tab[0] = true;
-        if(!etOpis.getText().toString().equals(""))
-            tab[1] = true;
         if(cbMamWydarzenie.isChecked()) {
             if(!etLink.getText().toString().equals("")){
-                tab[3] = true;
+                if(ifLinkWasChecked)
+                    tab[3] = true;
+                else {
+                    toast = Toast.makeText(AddEvent.this, "Wprowadź prawidłowy link lub zrezygnuj z niego.", Toast.LENGTH_SHORT);
+                    ((TextView)((LinearLayout)toast.getView()).getChildAt(0))
+                            .setGravity(Gravity.CENTER_HORIZONTAL);
+                    toast.show();
+                }
+
+            } else {
+                toast = Toast.makeText(AddEvent.this, "Wprowadź prawidłowy link lub zrezygnuj z niego.", Toast.LENGTH_SHORT);
+                ((TextView)((LinearLayout)toast.getView()).getChildAt(0))
+                        .setGravity(Gravity.CENTER_HORIZONTAL);
+                toast.show();
             }
         } else {
             tab[3] = true;
@@ -173,5 +222,21 @@ public class AddEvent extends AppCompatActivity {
         writer.write(start.toString());
         writer.flush();
         System.err.println(connection.getResponseCode());
+    }
+
+    public static boolean exists(String URLName){
+        try {
+            HttpURLConnection.setFollowRedirects(false);
+            // note : you may also need
+            //        HttpURLConnection.setInstanceFollowRedirects(false)
+            HttpURLConnection con =
+                    (HttpURLConnection) new URL(URLName).openConnection();
+            con.setRequestMethod("HEAD");
+            return (con.getResponseCode() == HttpURLConnection.HTTP_OK);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
