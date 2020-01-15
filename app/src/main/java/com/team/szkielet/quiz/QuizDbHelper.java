090447aa37ab61d2f338e2356555cfb653824f8b
ma7inowy <jakubwachala@wp.pl>
2020-01-15 19:03:53 +0100

    new database version

--- app/src/main/java/com/team/szkielet/quiz/QuizDbHelper.java
+++ app/src/main/java/com/team/szkielet/quiz/QuizDbHelper.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class QuizDbHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "PolitechnikaQuiz.db";
-    private static final int DATABASE_VERSION = 5;
+    private static final int DATABASE_VERSION = 6;
     private SQLiteDatabase db;
 
     public QuizDbHelper(Context context) {
