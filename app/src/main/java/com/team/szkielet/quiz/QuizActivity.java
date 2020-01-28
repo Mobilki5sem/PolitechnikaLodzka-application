f4ba49b210635931f74737e20ab51f025de1182f
Kar0lLasek <19karolasek98@gmail.com>
2020-01-28 19:30:13 +0100

    Do testowania losowanie odpowiedzi do pytań

--- app/src/main/java/com/team/szkielet/quiz/QuizActivity.java
+++ app/src/main/java/com/team/szkielet/quiz/QuizActivity.java
@@ -169,6 +169,95 @@ public class QuizActivity extends AppCompatActivity {
 
         if (questionCounter < questionTotal) {
             currentQuestion = questionList.get(questionCounter);
+            int correctAnswer = currentQuestion.getAnswerNr();
+            int newCorrectAnswer;
+            double randomDouble;
+            do {
+                randomDouble = Math.random();
+                randomDouble = randomDouble * 4 + 1;
+                newCorrectAnswer = (int) randomDouble;
+            } while (newCorrectAnswer == correctAnswer);
+
+            String answerA;
+            if(correctAnswer == 1) {
+                answerA = currentQuestion.getOption1();
+            } else if(correctAnswer == 2) {
+                answerA = currentQuestion.getOption2();
+            } else if (correctAnswer == 3) {
+                answerA = currentQuestion.getOption3();
+            } else {
+                answerA = currentQuestion.getOption4();
+            }
+            String answerB = "";
+            if(newCorrectAnswer == 1) {
+                answerB = currentQuestion.getOption1();
+                if(correctAnswer == 1) {
+                    //NIC
+                } else if(correctAnswer == 2) {
+                    currentQuestion.setOption2(answerB);
+                    currentQuestion.setOption1(answerA);
+                    currentQuestion.setAnswerNr(1);
+                } else if (correctAnswer == 3) {
+                    currentQuestion.setOption3(answerB);
+                    currentQuestion.setOption1(answerA);
+                    currentQuestion.setAnswerNr(1);
+                } else {
+                    currentQuestion.setOption4(answerB);
+                    currentQuestion.setOption1(answerA);
+                    currentQuestion.setAnswerNr(1);
+                }
+            } else if(newCorrectAnswer == 2) {
+                answerB = currentQuestion.getOption2();
+                if(correctAnswer == 1) {
+                    currentQuestion.setOption1(answerB);
+                    currentQuestion.setOption2(answerA);
+                    currentQuestion.setAnswerNr(2);
+                } else if(correctAnswer == 2) {
+                    //NIC
+                } else if (correctAnswer == 3) {
+                    currentQuestion.setOption3(answerB);
+                    currentQuestion.setOption2(answerA);
+                    currentQuestion.setAnswerNr(2);
+                } else {
+                    currentQuestion.setOption4(answerB);
+                    currentQuestion.setOption2(answerA);
+                    currentQuestion.setAnswerNr(2);
+                }
+            } else if (newCorrectAnswer == 3) {
+                answerB = currentQuestion.getOption3();
+                if(correctAnswer == 1) {
+                    currentQuestion.setOption1(answerB);
+                    currentQuestion.setOption3(answerA);
+                    currentQuestion.setAnswerNr(3);
+                } else if(correctAnswer == 2) {
+                    currentQuestion.setOption2(answerB);
+                    currentQuestion.setOption3(answerA);
+                    currentQuestion.setAnswerNr(3);
+                } else if (correctAnswer == 3) {
+                    //nic
+                } else {
+                    currentQuestion.setOption4(answerB);
+                    currentQuestion.setOption3(answerA);
+                    currentQuestion.setAnswerNr(3);
+                }
+            } else {
+                answerB = currentQuestion.getOption4();
+                if(correctAnswer == 1) {
+                    currentQuestion.setOption1(answerB);
+                    currentQuestion.setOption4(answerA);
+                    currentQuestion.setAnswerNr(4);
+                } else if(correctAnswer == 2) {
+                    currentQuestion.setOption2(answerB);
+                    currentQuestion.setOption4(answerA);
+                    currentQuestion.setAnswerNr(4);
+                } else if (correctAnswer == 3) {
+                    currentQuestion.setOption3(answerB);
+                    currentQuestion.setOption4(answerA);
+                    currentQuestion.setAnswerNr(4);
+                } else {
+                    //NIC
+                }
+            }
             question_txt.setText(currentQuestion.getQuestion());
             radioButton.setText(currentQuestion.getOption1());
             radioButton2.setText(currentQuestion.getOption2());
